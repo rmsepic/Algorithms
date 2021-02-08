cb2366bf76864314fb8c6fab85bbe7274f70600e
rmsepic <rmsepic@ucdavis.edu>
2021-02-08 14:04:22 -0800

    median of two sorted arrays does not work if the bounds of one array are around another.

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -24,59 +24,57 @@ class Solution:
                 i = len(nums1) // 2
                 return nums1[i]
 
-        tot_size = len(nums1) + len(nums2)
+        size = len(nums1) + len(nums2)
 
-        # Start with nums1
-        # If even
-        if (len(nums1) % 2) == 0:
-            i = len(nums1) // 2 - 1
-            j = len(nums1) // 2
+        # Set ranges
+        arr1 = nums1
+        arr2 = nums2
 
-            # If BOTH are even
-            if (len(nums2) % 2 == 0):
-                x = len(nums1) // 2 - 1
-                y = len(nums1) // 2
+        # Cut in half
+        i = len(nums1) // 2
+        x = len(nums2) // 2
 
-                if nums1[i] <= nums2[x] and nums1[j] <= nums2[y]:
-                    return (nums1[j] + nums2[x]) / 2
-                elif nums1[i] <= nums2[x] and nums1[j] >= nums2[y]:
-                    return (nums2[x] + nums2[y]) / 2
-                elif nums1[i] >= nums2[x] and nums1[j] <= nums2[y]:
-                    return (nums1[i] + nums1[j]) / 2
-                elif nums1[i] >= nums2[x] and nums1[j] >= nums2[y]:
-                    return (nums1[i] + nums2[y]) / 2
+        up_high = True
+        
+
+        while len(arr1) + len(arr2) > 2:
+            if arr1[i] > arr2[x]:
+                temp = arr1[i]
+                arr1 = arr1[:i]
+                arr2 = arr2[x:]
+                if len(arr1) != 1:
+                    arr1.append(temp)
+
+                up_high = True
+            elif arr1[i] < arr2[x]:
+                arr1 = arr1[i:]
+                temp = arr2[x]
+                arr2 = arr2[:x]
+                if len(arr2) != 1:
+                    arr2.append(temp)
+
+                up_high = False
             else:
-                x = len(nums2) // 2
+                return arr1[i]
+                
+            i = len(arr1) // 2
+            x = len(arr2) // 2
 
-                if nums1[i] >= nums2[x]:
-                    return nums1[i]
-                elif nums1[i] < nums2[x] and nums2[x] < nums1[j]:
-                    return nums2[x]
-                elif nums2[x] >= nums1[j]:
-                    return nums1[j]
-        # If odd
+            print("Arr1: ", arr1)
+            print("Arr2: ", arr2)
+
+        if size % 2 == 0:
+            return (arr1[0] + arr2[0]) / 2
         else:
-            i = len(nums1) // 2
-
-            if (len(nums2) % 2 == 0):
-                x = len(nums2) // 2 - 1
-                y = len(nums2) // 2
-
-                if nums1[i] <= nums2[x]:
-                    return nums2[x]
-                elif nums1[i] < nums2[y] and nums2[x] < nums1[i]:
-                    return nums1[i]
-                elif nums2[y] <= nums1[i]:
-                    print(nums2[x])
-                    return nums2[y]
-            # If BOTH are odd
+            if up_high == False:
+                return arr1[0]
             else:
-                x = len(nums2) // 2
-                return (nums1[i] + nums2[x]) / 2
+                return arr2[0]
+
 
 if __name__ == "__main__":
     nums1 = [1,2]
-    nums2 = [4,5,6]
+    nums2 = [-1,3]
     s = Solution()
     n = s.findMedianSortedArrays(nums1=nums1, nums2=nums2)
     print("answer: ", n)
