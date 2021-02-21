1bf93210ea29bedf40c8d292b935908280e30265
rmsepic <rmsepic@ucdavis.edu>
2021-02-21 15:12:35 -0800

    Close to a solution.

--- C/dining_philosophers.c
+++ C/dining_philosophers.c
@@ -1,46 +1,76 @@
+/*
+ * Using the arbitrator solution
+ */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <dispatch/dispatch.h>
-//#include <dispatch/sempahore.h>
 #include <dispatch/object.h>
+#include <dispatch/time.h>
 #include <stdbool.h>
 
 #define N 5 // Number of philosophers
+#define LEFT (*num - 1) % N
+#define RIGHT (*num + 1) % N
 
 bool eating[N] = {false, false, false, false, false};
-char *phil[N] = {"Plato", "Kant", "Sartre", "Kierkegaard", "Descartes"};
+const char *phil[N] = {"Plato", "Kant", "Sartre", "Kierkegaard", "Descartes"};
 
 dispatch_semaphore_t mutex;
 dispatch_semaphore_t sems[N];
 
-void take_left_fork() {
-
+void take_left_fork(int* num) {
+	printf("%s took a left fork\n", phil[*num]);
 }
 
-void take_right_fork() {
-
+void take_right_fork(int* num) {
+	printf("%s took a right fork\n", phil[*num]);
 }
 
-void eat() {
-
+void eat(int* num) {
+	printf("%s is eating\n", phil[*num]);
 }
 
-void put_left_fork() {
-
+void put_left_fork(int* num) {
+	printf("%s is done with left fork\n", phil[*num]);
 }
 
-void put_right_fork() {
-
+void put_right_fork(int *num) {
+	printf("%s is done with right fork\n", phil[*num]);
 }
 
 void philosopher(int* num) {
 	while(1) {
-		//dispatch_semaphore_wait(&sem);
+		dispatch_semaphore_wait(mutex, DISPATCH_TIME_FOREVER);
+		printf("Thread %u: %s is hungry. Num = %d\n", (unsigned)pthread_self(), phil[*num], *num);
+		
+		if (eating[LEFT] == false && eating[RIGHT] == false) {
+			take_right_fork(num);
+			take_left_fork(num);
+			eating[*num] = true;
+			eat(num);
+			dispatch_semaphore_signal(sems[*num]);
+		}
 
+		dispatch_semaphore_signal(mutex);
+		dispatch_semaphore_wait(sems[*num], DISPATCH_TIME_FOREVER);
+
+		dispatch_semaphore_wait(mutex, DISPATCH_TIME_FOREVER);
+		put_right_fork(num);
+		put_left_fork(num);
+		eating[*num] = false;
+		dispatch_semaphore_signal(mutex);
+		dispatch_semaphore_wait(sems[*num], DISPATCH_TIME_FOREVER);
 	}
 }
 
 int main() {
+	int phil_nums[N];
+
+	for (int i = 0; i < N; i++) {
+		phil_nums[i] = i;
+	}
+
 	pthread_t id[N];
 
 	mutex = dispatch_semaphore_create(1);
@@ -49,6 +79,11 @@ int main() {
 	}
 
 	for (int i = 0; i < N; i++) {
-		pthread_create(&id[0], NULL, (void*)philosopher, &i);
+		pthread_create(&id[i], NULL, (void*)philosopher, (void*)&phil_nums[i]);
+		printf("Thread %u: %s is dining\n", (unsigned)id[i], phil[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		pthread_join(id[i], NULL);
 	}
 }
--- C/threads.c
+++ C/threads.c
@@ -3,12 +3,15 @@
 #include <pthread.h>
 
 void foo(void *t) {
-	printf("Thread ID: %d\n", (int)t);
+	printf("Thread ID: %u\n", (unsigned)t);
+	printf("pthread self: %u\n", (unsigned)pthread_self());
 }
 
 int main() {
 	pthread_t id1, id2;
 	pthread_create(&id1, NULL, (void*)foo, &id1);
+	printf("Thread1 %u\n", (unsigned)id1);
 	pthread_create(&id2, NULL, (void*)foo, &id2);
-	printf("Thread %d\n", (int)id1);
+	printf("Thread2 %d\n", (int)id2);
+	
 }
