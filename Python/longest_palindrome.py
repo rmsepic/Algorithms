8f1c7e6d9b71f7ed33f8a99b4db2c15ae20622d2
rmsepic <rmsepic@ucdavis.edu>
2021-02-23 11:35:24 -0800

    Brute force longest palindrome solution

---
+++ Python/longest_palindrome.py
@@ -0,0 +1,22 @@
+def brute_force(s: str) -> str:
+	max_len = 1
+	start = 0
+
+	# Check all start and end position of each possible palindrome
+	for i in range(0, len(s)):
+		for j in range(i, len(s)):
+			flag = True
+			for k in range(0, ((j - i) // 2) + 1):
+				if (s[i + k] != s[j - k]):
+					flag = False
+
+			if flag == True and (j - i + 1) > max_len:
+				max_len = j - i + 1
+				start = i
+
+	return s[start:start + max_len]
+
+if __name__ == "__main__":
+	s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"
+	ans = brute_force(s)
+	print(ans)
