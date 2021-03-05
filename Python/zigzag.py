522923470f5b247009dddd3536ecf0754d430fca
rmsepic <rmsepic@ucdavis.edu>
2021-03-04 20:57:23 -0800

    Zigzag works for 3 rows.

--- Python/zigzag.py
+++ Python/zigzag.py
@@ -1,17 +1,17 @@
 def convert(s: str, rows: int):
     ans = ""
+    rows_ = rows
+    x = rows_ * 2 - 3 + 1
 
-    idx = 0
-    x = 1
-    
-    while len(ans) < len(s):
-        print(idx)
-        ans += s[idx]
-        idx += rows + 1
-        if idx >= len(s):
-            idx = x 
-            x += 1
+    for r in range(0, rows):
+        for a in range(r, len(s), x):
+            ans += s[a]
 
+        rows_ -= 1
+        x = rows_ * 2 - 3 + 1
+        print(r, x)
+        if r == 1:
+            x = rows * 2 - 3 + 1
 
     return ans
 
