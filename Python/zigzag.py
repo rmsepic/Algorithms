b86cc67463fea2a78f5b91f9b0f4d82f463267ea
rmsepic <rmsepic@ucdavis.edu>
2021-03-04 21:36:58 -0800

    Completed zigzag

--- Python/zigzag.py
+++ Python/zigzag.py
@@ -1,34 +1,40 @@
 # Returns the arrays of the distance between the indexes per row
 def calcArr(rows: int) -> list:
-    arr = [-1 for _ in range(rows)]
-    index = rows * 2 - 3 
+    arr = [(1, 1) for _ in range(rows)]
+    left = rows * 2 - 3 
+    right = 1
 
-    for r in range(0, (rows + 1) // 2 ):
-        arr[r] = index 
-        arr[rows - r - 1] = index  
-        index -= 2
+    arr[0] = (left, left)
+    arr[rows - 1] = (left, left)
 
+    left -= 2
+
+    for r in range(1, (rows + 1) // 2 ):
+        arr[r] = (left, right)
+        arr[rows - r - 1] = (right, left)  
+        left -= 2
+        right += 2
+        
     return arr
 
 def convert(s: str, rows: int):
+    if rows == 1:
+        return s
+
     ans = ""
-    r = 0
+    r = x = 0
     arr = calcArr(rows)
     print(arr)
-
     for a in arr:
-        for x in range(r, len(s), a + 1):
+        x = r
+        b = 0
+        while x < len(s):
             ans += s[x]
+            x += a[b] + 1
+            b = 1 - b
 
         r += 1
 
     return ans
 
 
-if __name__ == "__main__":
-    s = "PAYPALISHIRING"
-    ans = "PAHNAPLSIIGYIR"
-    rows = 3
-
-    print(convert(s, rows))
-    print(convert(s, rows) == ans)
