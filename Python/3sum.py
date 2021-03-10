35ff0561b309ec09a0c232ab34b40b3744a3946e
rmsepic <rmsepic@ucdavis.edu>
2021-03-10 15:49:50 -0800

    Finished 3sum

--- Python/3sum.py
+++ Python/3sum.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def threeSum(nums: List[int]) -> List[List[int]]:
+def threeSum_unique_duplicates(nums: List[int]) -> List[List[int]]:
     ans = []
     dic = {}
     size_ = len(nums)
@@ -15,7 +15,36 @@ def threeSum(nums: List[int]) -> List[List[int]]:
 
     return ans 
 
+def threeSum(nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    print(nums)
+    ht = {}
+    ans = []
+
+    for i, val in enumerate(nums):
+        ht[val] = i
+
+    print(ht)
+
+    for i in range(0, len(nums) - 1):
+        if i - 1 >= 0 and nums[i] == nums[i - 1]: 
+            continue
+        else:
+            for j in range(i + 1, len(nums)):
+                if j - 1 >= i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                else:
+                    target = -1 * (nums[i] + nums[j])
+
+                    if target in ht and ht[target] > j:
+                        ans.append([nums[i], nums[j], target])
+
+    return ans
+
+
+
+
 if __name__ == "__main__":
-    l = [-1,0,1,2,-1,-4]
+    l = [0,0,0]
     ans = threeSum(l)
     print(ans)
