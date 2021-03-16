2b97d89ca07b978c7cd49f53f08acee770b57365
rmsepic <rmsepic@ucdavis.edu>
2021-03-16 11:38:16 -0700

    Langford pair finished.

--- Python/langford_pairing.py
+++ Python/langford_pairing.py
@@ -1,11 +1,14 @@
+# Recurssive and iterative solutions for generating a 
+# Langford paired array 
+# Only works for numbers that have Langford pairs
+# Does not work for N = 5, N = 6
+
 from typing import List
 
 def lang(N: int, arr: List, index: int, ht: dict):
     if N == 0:
         return arr
     
-    print(arr, N, index, index + N + 1)
-
     # Place the next one
     if index + N + 1 < len(arr) and arr[index] == -1 and arr[index + N + 1] == -1:
         arr[index] = N 
@@ -41,6 +44,17 @@ def langford(N: int):
 # _,_,_,_,_,_,_,_,_,_
 # 6,4,5,_,_,_,4,6,5,_,_,_
 
+def helper(N: int, arr: List, start: int, ht: dict):
+    for index in range(start, len(arr) - N - 1):
+        # If the path is clear
+        if arr[index] == -1 and arr[index + N + 1] == -1:
+            arr[index] = N 
+            arr[index + N + 1] = N
+            ht[N] = index
+            return True 
+
+    return False
+
 def langford_loop(N: int):
     arr = [-1 for i in range(0, 2 * N)]
     ht = {}
@@ -49,29 +63,22 @@ def langford_loop(N: int):
 
     # Loop through numbers and place them
     while N > 0:
-        print(arr)
-        flag = False
-        for index in range(start, len(arr) - N - 1):
-            # If the path is clear
-            if arr[index] == -1 and arr[index + N + 1] == -1:
-                arr[index] = N 
-                arr[index + N + 1] = N
-                ht[N] = index
-                flag = True
-        
-        # Could not place N
-        # Have to go to N + 1 and place it somewhere else
-        if flag == False:
-            N += 1 
-            print(ht)
-            print(N)
-            start = ht[N]
-            ht.pop(N)
-        else:
+        print(arr, N)
+           
+        if helper(N, arr, start, ht) == True:
             N -= 1
             start = 0
+        # Could not place N
+        # Have to go to N + 1 and place it somewhere else
+        else:
+            N += 1 
+            arr[ht[N]] = -1
+            arr[ht[N] + N + 1] = -1
+            start = ht[N] + 1
+            ht.pop(N)
+           
 
-    return True
+    return arr
 
     
 # Checks if the array is a valid Langford Pair    
@@ -87,13 +94,13 @@ def check(arr: List) -> bool:
 
 
 if __name__ == "__main__":
-    N = 6
+    N = 8
     ans = langford_loop(N)
     print(ans)
     print(check(ans))
-   # ans_loop = langford_loop(N)
-  #  print(ans, ans_loop)
+    ans_loop = langford_loop(N)
+    print(ans, ans_loop)
 
- #   print(ans == ans_loop)
+    print(ans == ans_loop)
 
-#    print(check(ans_loop))
+    print(check(ans_loop))
