29e3b7a0c94557e1cebc111bdee35cda85ff8231
rmsepic <rmsepic@ucdavis.edu>
2021-03-20 11:46:36 -0700

    Regex attempt.

--- Python/regex.py
+++ Python/regex.py
@@ -13,8 +13,10 @@ def isMatch(p: str, s: str) -> bool:
     j = 0
     prev = None
     right = None
-
-    for i, norm in enumerate(s):
+    i = 0
+    while i < len(s) and j < len(p):
+        print("Pointers ", s[i], p[j], prev, right)
+        norm = s[i]
         # Given 'x*y'
         # prev is x
         # right is y
@@ -23,29 +25,31 @@ def isMatch(p: str, s: str) -> bool:
                 prev = '.'
                 
                 if i + 2 < len(s):
-                    right = [i + 2]
+                    right = s[i + 2]
             else:
                 prev = norm
                 
                 if i + 2 < len(s):
-                    right = [i + 2]
+                    right = s[i + 2]
 
         # For non special cases
         if norm == p[j]:
             j += 1
-        elif norm == '.':
+            i += 1
+        elif norm == '.' and prev == None:
+            j += 1
+            i += 1
+        # x is equal to the next in j
+        elif prev == p[j] or prev == '.':
             j += 1
         # right is equal to j
         # Get out of the '*' special case and read the string as normal
-        elif right == p[j]:
+        elif right == p[j] or right == '.':
             j += 1
             prev = None
             right = None
-        # x is equal to the next in j
-        elif prev == p[j]:
-            j += 1
         else:
-            return False
+            i += 1
 
     if j == len(p):
         return True
@@ -62,10 +66,12 @@ def isMatch(p: str, s: str) -> bool:
 # "aaaaaaa"
 
 if __name__ == "__main__":
-    a = ["a*b*", "a*b*", "a*", "c*a*b", "a"]
-    b = ["aaaaaaa", "aaaaaaa", "aa", "aab", "aa"]
-    valid = [True, True, True, True, False]
+    a = ["a*b*", "a*b*", "a*", "c*a*b", "a", ".*", "mis*is*ip*.",       "mis*is*p*."]
+    b = ["aaaaaaa", "aaaaaaa", "aa", "aab", "aa", "ab", "mississippi",  "mississippi"]
+    valid = [True, True, True, True, False, True, True, False]
 
+    #i = 6
+    #print(valid[i], "\t", isMatch(b[i], a[i]))
     for i in range(0, len(a)):
         print(valid[i], "\t", isMatch(b[i], a[i]))
 
