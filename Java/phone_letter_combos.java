0a975c36ff48483daea7dc7bced3973210e93f38
rmsepic <rmsepic@ucdavis.edu>
2021-03-26 19:15:39 -0700

    Phone number letters combination problem complete.

--- Java/phone_letter_combos.java
+++ Java/phone_letter_combos.java
@@ -1,30 +1,9 @@
+// Leetcode problem
+
 class Solution {
+    private Map<Character, String> buttons = new HashMap<Character, String>();
     
-    
-    public String dfs(Map<Character, String> buttons, String remaining_digits) {
-        if (remaining_digits.length() == 0) {
-            return "";
-        }
-        
-        List<String> permutations = new ArrayList<String>();
-        String letters = buttons.get(digits.charAt(0));
-        for (int i = 0; i < letter.length(); i++) {
-            String str = digits.charAt(j) + dfs(buttons, digits.substring(1));
-            permutations.add(str);         
-        }
-        
-        return permutations;
-        }
-    }
-    
-    public List<String> letterCombinations(String digits) {
-        List<String> permutations = new ArrayList<String>();
-        
-        if (digits.length() == 0) {
-            return permutations;
-        }
-        
-        Map<Character, String> buttons = new HashMap<Character, String>();
+    public void initialize_buttons() {
         buttons.put('2', "abc");
         buttons.put('3', "def");
         buttons.put('4', "ghi");
@@ -33,13 +12,44 @@ class Solution {
         buttons.put('7', "pqrs");
         buttons.put('8', "tuv");
         buttons.put('9', "wxyz");
-            
-        // Phone letters 'abc', 'def', etc.
-        String letters = buttons.get(digits.charAt(0)); 
-        for (int j = 0; j < letters.length(); j++) {
-            String str = digits.charAt(j) + dfs(buttons, digits.substring(1));
-            permutations.add(str);         
+    }
+    
+    public List<String> dfs(List<String> level, String remaining_digits) {
+        // If empty return empty
+        if (remaining_digits.length() == 0) {
+            return level;
         }
+
+        String letters = buttons.get(remaining_digits.charAt(0));
+        List<String> next_level = new ArrayList<String>();
+        
+        for (int i = 0; i < level.size(); i++) {
+            for (int j = 0; j < letters.length(); j++) {
+                next_level.add(level.get(i) + Character.toString(letters.charAt(j)));
+            }
+        }
+        
+        return dfs(next_level, remaining_digits.substring(1));
+    }
+    
+    // Main function
+    public List<String> letterCombinations(String digits) {
+        List<String> permutations = new ArrayList<String>();
+        List<String> first_level = new ArrayList<String>();
+        
+        if (digits.length() == 0) {
+            return permutations;
+        }
+        
+        initialize_buttons();
+        
+        // First set of letters
+        String letters = buttons.get(digits.charAt(0));
+        for (int i = 0; i < letters.length(); i++) {
+            first_level.add(Character.toString(letters.charAt(i)));
+        }
+        
+        permutations = dfs(first_level, digits.substring(1));
         
         return permutations;
     }
