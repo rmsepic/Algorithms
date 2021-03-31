2a10507e0e4bd55b6e554b15cb4483e4bef47ef2
rmsepic <rmsepic@ucdavis.edu>
2021-03-31 11:36:39 -0700

    Working sudoku solution.

--- Python/sudoku_solver.py
+++ Python/sudoku_solver.py
@@ -18,7 +18,7 @@ class Sudoku:
 
     def __init__(self, board: List[List[Cell]]):
         self.board = board
-
+        
     def dfs(self, row: int, col: int) -> bool:
         for i in range(row, self.LENGTH):
             col = 0
@@ -26,23 +26,32 @@ class Sudoku:
                 if (self.board[i][j] == Cell.EMPTY.value):
                     # Try to place 1-9 in the empty cell
                     for s in Cell:
-                        if s.value not in board[i] and s.value not in self.getCol(j):
+                        if s.value not in self.board[i] and s.value not in self.getCol(j) and self.notInBox(s.value, i,j):
                             self.board[i][j] = s.value
                             flag = self.dfs(i, j)
 
                             if flag is True:
                                 return True
-
+ 
                             self.board[i][j] = Cell.EMPTY.value
 
                     return False
 
         return True
-                       
-
 
     def getCol(self, col: int) -> List[Cell]:
         return [row[col] for row in self.board]
+
+    def notInBox(self, value: int, i: int, j: int) -> bool:
+        row = i - i % 3
+        col = j - j % 3
+
+        for x in range(row, row + 3):
+            for y in range(col, col + 3):
+                if (self.board[x][y] == value):
+                    return False 
+
+        return True
 
     def printBoard(self):
         for row in self.board:
@@ -58,6 +67,7 @@ class Sudoku:
 
 
 if __name__ == "__main__":
+    #board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
     board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
     Sudoku.solve(board)
 
