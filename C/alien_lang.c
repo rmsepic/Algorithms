5b8bcaf4d30e328595d71b9817a77dfbde7b3c1c
rmsepic <rmsepic@ucdavis.edu>
2021-04-09 18:53:02 -0700

    Alien lang

--- C/alien_lang.c
+++ C/alien_lang.c
@@ -1,4 +1,38 @@
-//bool compareRest 
+
+
+// Left word is the word left in the order
+
+bool compareRest(char *left, char *right, char *order, int order_idx) {
+    printf("Left: %s    Right: %s\n", left, right);
+    
+    // If the left and right words were the same
+    if (left[0] == '\0' && right[0] == '\0') {
+        return true;
+    } else if (left[0] == '\0') {
+        return true;
+    } else if (right[0] == '\0') {
+        return false;
+    }
+    
+    if (left[0] == right[0]) {
+        return compareRest(++left, ++right, order, order_idx);
+    } else {
+        // They don't match so make sure they are in order         
+        while (left[0] != order[order_idx] && right[0] != order[order_idx]) {
+            order_idx++;
+            // Overflowed the order
+            if (order_idx >= 26) {
+                return false;
+            }
+        }
+        
+        if (right[0] == order[order_idx]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 bool isAlienSorted(char ** words, int wordsSize, char * order){
     // Only one word so it has to be sorted
@@ -10,26 +44,22 @@ bool isAlienSorted(char ** words, int wordsSize, char * order){
     int word_idx = 0;
     
     for (; word_idx + 1 < wordsSize; word_idx++) {
-        printf("%s %s\n", words[word_idx], words[word_idx + 1]);
         // If they match
-        if (strcmp(words[word_idx], words[word_idx + 1]) == 0) {
-           // if (compareRest(word, next, ord) == false){
-           //     return false;
-           // }            
+        if (words[word_idx][0] == words[word_idx + 1][0]) {
+            if (compareRest(words[word_idx], words[word_idx + 1], order, order_idx) == false){
+                return false;
+            }            
         } else {
-            // They don't match so make sure they are in order
-            if (words[word_idx][0] == order[order_idx]) {
-                // In the correct order
-                // Update the order 
-                while (words[word_idx + 1][0] != order[order_idx]) {
-                    order_idx++;
-                    // Overflowed the order
-                    if (order_idx >= 26) {
-                        return false;
-                    }
-                    
-                    //printf("ord %c %c\n", words[word_idx][0], order[order_idx]);
-                }
+            while (words[word_idx][0] != order[order_idx] && words[word_idx + 1][0] != order[order_idx]) {
+                order_idx++;
+                // Overflowed the order
+                if (order_idx >= 26) {
+                    return false;
+                }                    
+            }
+            
+            if (words[word_idx + 1][0] == order[order_idx]) {
+                return false;
             }
         }
     }
